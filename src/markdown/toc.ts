7f068a734236139aa4e6a168a869c37cf23411fa
Mirion Lee <mirion@qq.com>
2023-04-25 18:00:01 +0800

    更换图标，样式小修

--- src/markdown/toc.ts
+++ src/markdown/toc.ts
@@ -3,10 +3,12 @@ import { registerPlugin } from "@yank-note/runtime-api"
 const name = 'extension-math.markdown.toc'
 
 export default () => {
-	registerPlugin({
-		name,
-		register: ctx => {
-
-		}
-	})
+    registerPlugin({
+        name,
+        register: ctx => {
+            ctx.markdown.registerPlugin(md => {
+                // md.block.ruler.getRules('heading')
+            })
+        }
+    })
 }
