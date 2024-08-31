cc6ace26178c2243eeea6a28e2fafa6cd9edf17b
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-08-31 14:46:29 +0800

    add default env

--- src/markdown-theorem-environment.ts
+++ src/markdown-theorem-environment.ts
@@ -7,6 +7,73 @@ export type EnvironmentData = {
 const defaultStyle = `
 `
 
+const defaultEnv = {
+    axiom: {
+        text: '公理',
+        counter: 1
+    },
+    definition: {
+        text: '定义',
+        counter: 2
+    },
+    theorem: {
+        text: '定理',
+        counter: 3
+    },
+    proposition: {
+        text: '命题',
+        counter: 'theorem'
+    },
+    lemma: {
+        text: '引理',
+        counter: 'theorem'
+    },
+    corollary: {
+        text: '推论',
+        counter: 'theorem'
+    },
+    property: {
+        text: '性质',
+        counter: 'theorem'
+    },
+    problem: {
+        text: '问题',
+        counter: 3
+    },
+    example: {
+        text: '例',
+        counter: 3
+    },
+    exercise: {
+        text: '练习',
+        counter: 3
+    },
+    note: {
+        text: '笔记',
+        counter: 0
+    },
+    conclusion: {
+        text: '结论',
+        counter: 0
+    },
+    assumption: {
+        text: '假设',
+        counter: 0
+    },
+    remark: {
+        text: '注',
+        counter: 0
+    },
+    solution: {
+        text: '解',
+        counter: 0
+    },
+    proof: {
+        text: '证明',
+        counter: 0
+    }
+}
+
 export default class Environment {
     style: HTMLStyleElement
     template: string
@@ -19,7 +86,7 @@ export default class Environment {
     constructor(style: HTMLStyleElement) {
         this.style = style
         this.template = ''
-        this.env = {}
+        this.env = defaultEnv
     }
 
     /**
