475772d6782ae5d3197a1488d10985def8d8d7ab
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-09-05 00:22:46 +0800

    spin off the background module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import background from './background'
 import markdownKatex from './markdown-katex'
 import markdownTheorem from './markdown-theorem'
 import replacePunctuation from './replace-punctuation'
 
 [
-    background,
     markdownKatex,
     markdownTheorem,
     replacePunctuation
